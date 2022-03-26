919e024ad59dfe997a9afe65dc6df913d0815851
tinnguyen0812 <51200795+tinnguyen0812@users.noreply.github.com>
2022-03-26 23:31:06 +0700

    .

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,8 +1,7 @@
 const category = require("../models/category");
 const subCategory = require("../models/sub-category");
 const { StatusCodes } = require("http-status-codes");
-const res = require("express/lib/response");
-const { error } = require("@hapi/joi/lib/base");
+
 class CategoryController {
   async List(req, res) {
     try {

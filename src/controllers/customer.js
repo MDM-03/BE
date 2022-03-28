97b432ea695b02dce8148fcf6b2a5aef57230e2e
tinnguyen0812 <51200795+tinnguyen0812@users.noreply.github.com>
2022-03-28 23:53:29 +0700

    .

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -13,5 +13,22 @@ class CustomerController {
       return err;
     }
   }
+  async Assign(req, res) {
+    try {
+      const body = req.body;
+      const key = Object.key(body);
+      const ID = "";
+      const data = customer.findById({ _id: body._id });
+      if (!data) {
+        return res.status(StatusCodes.NOT_FOUND).send("Not found customer");
+      }
+      key.foreach((update) => (data[update] = body[update]));
+      await data.save();
+    } catch (err) {
+      res.status(StatusCodes.BAD_REQUEST).send("Server error");
+      console.log(err);
+      return err;
+    }
+  }
 }
 module.exports = new CustomerController();
